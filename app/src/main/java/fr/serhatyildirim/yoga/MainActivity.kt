fac83676234037c3576658640230311c25b66bbb
Serhat Yildirim <serhat.yildirim@hetic.net>
2018-05-31 02:40:56 +0200

    add second activity

--- app/src/main/java/fr/serhatyildirim/yoga/MainActivity.kt
+++ app/src/main/java/fr/serhatyildirim/yoga/MainActivity.kt
@@ -1,12 +1,40 @@
 package fr.serhatyildirim.yoga
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+    fun toastMe(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun randomMe(view: View){
+        // Create an Intent to start the second activity
+        val randomIntent = Intent(this, SecondActivity::class.java)
+
+        // Start the new activity.
+        startActivity(randomIntent)
+    }
+
+
+
+
+
+    // Create an Intent to start the second activity
+
 }
---
+++ app/src/main/java/fr/serhatyildirim/yoga/SecondActivity.kt
@@ -0,0 +1,17 @@
+package fr.serhatyildirim.yoga
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+
+
+
+
+}
