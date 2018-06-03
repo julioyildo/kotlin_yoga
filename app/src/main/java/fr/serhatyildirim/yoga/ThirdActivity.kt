d9c03b214051f680ffb02df91165a23d062d56a8
Serhat Yildirim <serhat.yildirim@hetic.net>
2018-06-03 03:15:21 +0200

    fix(thidActivity): deffin all elements nullable

--- app/src/main/java/fr/serhatyildirim/yoga/ThirdActivity.kt
+++ app/src/main/java/fr/serhatyildirim/yoga/ThirdActivity.kt
@@ -1,44 +1,103 @@
 package fr.serhatyildirim.yoga
 
+
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
-import java.util.*
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+
 // Android extensions import statements for the two views to update
-import kotlinx.android.synthetic.main.activity_third.title_music
 
 class ThirdActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+
+        //showRandomNumber()
+        mediaPlayer = MediaPlayer.create(this, R.raw.meditation_music)
+        leftTime = findViewById(R.id.currentTime)
+        rightTime = findViewById(R.id.duration)
+        prevButton = findViewById<Button>(R.id.prevButton)
+        playButton = findViewById<Button>(R.id.play_btn)
+        nextButton = findViewById<Button>(R.id.nextButton)
+        seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+
+    }
+
+    private var mediaPlayer: MediaPlayer? =null
+    private var leftTime:TextView? = null
+    private var rightTime: TextView? = null
+    private var seekBar: SeekBar? = null
+    private var prevButton:Button? = null
+    private var playButton:Button? = null
+    private var nextButton:Button? = null
+
+
+
+    //init {
+
+       // this.mediaPlayer = MediaPlayer.create(this, R.raw.meditation_music)
+       // this.leftTime   = findViewById(R.id.currentTime)
+        //this.rightTime  = findViewById(R.id.duration)
+        //this.playButton = findViewById(R.id.play_btn)
+
+        //this.nextButton = findViewById(R.id.nextButton)
+        //this.seekBar    = findViewById(R.id.seekBar)
+
+
+
+   // }
 
 
     companion object {
         val EXTRA_VALUE = "EXTRA_VALUE"
-
-        val tets = {
-
-        }
 
         fun createIntent(context: Context, value: Int): Intent {
             return Intent(context, ThirdActivity::class.java).putExtra(EXTRA_VALUE, value)
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_third)
 
-        showRandomNumber()
+
+
+   /* private fun clickMenu(view: TextView) {
+
+        when(view) {
+            prevButton->{
+
+            }
+            playButton -> {
+
+            }
+
+            nextButton -> {
+
+            }
+
+
+
+        }
     }
+*/
+    //public void pauseMusic() {
 
+    //}
 
+    //public void startMusic() {
 
-
+    //}
 
 //Other class code...
 
     fun showRandomNumber() {
         // Get the count from the intent extras
-        val count = intent.getIntExtra(EXTRA_VALUE, 0)
+       // val count = intent.getIntExtra(EXTRA_VALUE, 0)
 
         // Generate the random number
        /* val random = Random()
@@ -50,10 +109,12 @@ class ThirdActivity : AppCompatActivity() {
         }
 */
         // Display the random number.
-        title_music.text = count.toString()
+       // title_music.text = count.toString()
 
         // Substitute the max value into the string resource
         // for the heading, and update the heading
         /*textview_label.text = getString(R.string.random_heading, count)*/
     }
+
+
 }
