07b0621634831d0abb0217bb3f13b79f929f83a4
Serhat Yildirim <serhat.yildirim@hetic.net>
2018-06-07 03:48:48 +0200

    add a lots off player things

--- app/src/main/java/fr/serhatyildirim/yoga/ThirdActivity.kt
+++ app/src/main/java/fr/serhatyildirim/yoga/ThirdActivity.kt
@@ -5,53 +5,75 @@ import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.provider.MediaStore
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
-import android.widget.Toast
+import fr.serhatyildirim.yoga.R.id.seekBar
+import java.lang.reflect.Array
 import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
 import java.util.*
+import kotlin.collections.ArrayList
 
 // Android extensions import statements for the two views to update
+data class Music(var name: String, var song: MediaPlayer, var position: Int)
 
  class ThirdActivity : AppCompatActivity() {
 
-
+     private var titleMusic: TextView? = null
+     private var currentSession: TextView? = null
+     //private var  currentMusicInformations:  =null
      private var mediaPlayer: MediaPlayer? = null
-    private var leftTime:TextView? = null
-    private var rightTime: TextView? = null
-    private var seekBar: SeekBar? = null
-    private var prevButton:Button? = null
-    private var playButton:Button? = null
-    private var nextButton:Button? = null
+     private var leftTime:TextView? = null
+     private var rightTime: TextView? = null
+     private var seekBar: SeekBar? = null
+     private var prevButton:Button? = null
+     private var playButton:Button? = null
+     private var nextButton:Button? =null
+     private var handler: Handler? = null
+
+
+     private var musics: ArrayList<Music>? = null
+     private var index: Int = 0
+
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
 
-        //showRandomNumber()
-        mediaPlayer = MediaPlayer.create(this, R.raw.zaza)
-        leftTime = findViewById(R.id.currentTime)
-        rightTime = findViewById(R.id.duration)
+
+        this.musics = arrayListOf(
+                Music("Pour un beau réveille",  MediaPlayer.create(this, R.raw.matinal), 0),
+                Music("Avant une bonne nuit",  MediaPlayer.create(this, R.raw.soiree), 1)
+        )
+
+
+        titleMusic = findViewById<TextView>(R.id.title_music)
+        currentSession = findViewById<TextView>(R.id.track)
+        mediaPlayer = this.musics!![this.index].song
+        leftTime = findViewById<TextView>(R.id.currentTime)
+        rightTime = findViewById<TextView>(R.id.duration)
         prevButton = findViewById<Button>(R.id.prevButton)
         playButton = findViewById<Button>(R.id.play_btn)
         nextButton = findViewById<Button>(R.id.nextButton)
         seekBar = findViewById<SeekBar>(R.id.seekBar)
-
+        handler = Handler()
         playButton?.setOnClickListener{clickMenu(playButton!!)}
-       // prevButton?.setOnClickListener(this)
-       // nextButton?.setOnClickListener(this)
+        prevButton?.setOnClickListener{clickMenu(prevButton!!)}
+        nextButton?.setOnClickListener{clickMenu(nextButton!!)}
+
 
 
         seekBar?.max = mediaPlayer?.duration!!
-        seekBar?.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
 
+
+
+        seekBar?.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
                 // Display the current progress of SeekBar
                 if (b){
@@ -78,9 +100,10 @@ import java.util.*
         })
 
 
-
-
+        this.setInformation()
     }
+
+
 
 
 
@@ -94,15 +117,40 @@ import java.util.*
 
 
 
+
+
+
+     // GET MUSIC
+     private fun getMusic(){
+         this.mediaPlayer = this.musics!![this.index].song
+
+     }
+
+
+
+
+
+
+
+
+     // HANDLE BUTTONS CLICKS
      private fun  clickMenu(view: View) {
 
          when(view) {
             prevButton->{
+                this.pauseMusic()
+                this.index = this.index - 1
+                this.setSeek()
+                this.getMusic()
+                this.startMusic()
+                this.setInformation()
 
             }
             playButton -> {
+                this.getMusic()
                 if(mediaPlayer?.isPlaying!!) {
                     this.pauseMusic()
+
 
                 } else {
                     this.startMusic()
@@ -111,6 +159,12 @@ import java.util.*
             }
 
             nextButton -> {
+                this.pauseMusic()
+                this.index = this.index +1
+                this.setSeek()
+                this.getMusic()
+                this.startMusic()
+                this.setInformation()
 
             }
 
@@ -119,6 +173,10 @@ import java.util.*
 
 
 
+
+
+
+     //Pause Music
    private fun pauseMusic(){
         if (mediaPlayer != null){
            mediaPlayer?.pause()
@@ -128,36 +186,82 @@ import java.util.*
 
 
 
-    private fun startMusic() {
+
+
+
+     // Start Music
+     private fun startMusic() {
         if (mediaPlayer != null){
             mediaPlayer?.start()
             playButton?.setBackgroundResource(android.R.drawable.ic_media_pause)
+            this.updateThread()
+            this.setInformation()
         }
-    }
+     }
 
-//Other class code...
 
-    //fun showRandomNumber() {
-        // Get the count from the intent extras
-       // val count = intent.getIntExtra(EXTRA_VALUE, 0)
 
-        // Generate the random number
-       /* val random = Random()
-        var randomInt = 0
-        // Add one because the bound is exclusive
-        if (count > 0) {
-            // Add one because the bound is exclusive
-            randomInt = random.nextInt(count + 1)
-        }
-*/
-        // Display the random number.
-       // title_music.text = count.toString()
 
-        // Substitute the max value into the string resource
-        // for the heading, and update the heading
-        /*textview_label.text = getString(R.string.random_heading, count)*/
-   // }
+
+
+
+     // update Thread
+     private fun updateThread(){
+         seekBar?.setProgress(mediaPlayer?.currentPosition!!)
+
+         if(mediaPlayer?.isPlaying!!){
+            var runnable = Runnable {
+                 kotlin.run {
+                     this.updateThread()
+                 }
+
+             }
+             handler?.postDelayed(runnable, 1000)
+
+         }
+
+     }
+
+
+
+
+
+
+    // Previous Music
+     private fun setSeek() {
+             mediaPlayer?.seekTo(0)
+
+     }
+
+
+
+
+
+
+
+     // Next Music
+     private fun nextMusic() {
+        if (mediaPlayer?.isPlaying!!) {
+             mediaPlayer?.seekTo(mediaPlayer?.duration!!)
+         }
+     }
+
+
+
+
+
+
+
+
+     // Set Title and Session TextView
+     private fun setInformation() {
+         this.titleMusic?.text = this.musics!![this.index].name
+         this.currentSession?.text = "Session : " + (this.musics!![this.index].position +1) + " / " + this.musics?.size
+
+     }
 
 
 }
+
+
 
